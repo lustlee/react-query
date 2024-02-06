49c3b4d47a4892c8c0c61ef945821b3759d8d0d0
Ramazan Tokoshev <r.tokoshev@gmail.com>
2024-02-06 18:07:17 +0600

    delete css files, add bootstrap, render data React-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,75 @@
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { CoinsTable } from './components/CoinsTable';
-import { DataCoins } from './constants';
+import axios from 'axios';
+import { useQuery } from 'react-query';
+
+const options = {
+	method: 'GET',
+	headers: {
+		accept: 'application/json',
+		'X-API-KEY': 'fNUklG+BQISL2ME2nbgcUBSdynmo5+CShTAPZERpZSE=',
+	},
+};
+
+// TODO: Новый подход с React-query.
+const fetchCoins = async () => {
+	const { data } = await axios.get(
+		'https://openapiv1.coinstats.app/coins?limit=10',
+		options
+	);
+
+	return data.result;
+};
+
+// TODO: Более классический вариант, как бы мы делали запрос на сервер.
+// const fetchCoins = async () => {
+// 	try {
+// 		const { data } = await axios.get(
+// 			'https://openapiv1.coinstats.app/coins?limit=10',
+// 			options
+// 		);
+// 		setCoins(data.coins);
+// 	} catch (error) {
+// 		setError(true);
+// 	} finally {
+// 		setLoading(false);
+// 	}
+// };
 
 function App() {
+	// TODO: Нам даже не нужны state для работы.
+	// const [coins, setCoins] = useState([]);
+	// const [loading, setLoading] = useState(true);
+	// const [error, setError] = useState(false);
+
+	// TODO: Даже этот хук Эффекта нам не нужен.
+	// useEffect(() => {
+	// 	void fetchCoins();
+	// }, []);
+
+	//                                        Ключ запроса, Сама функция запроса
+	const { isLoading, isError, data } = useQuery('coins', fetchCoins);
+
+	if (isLoading) {
+		return <h3>Loading ...</h3>;
+	}
+
+	if (isError) {
+		return <h3>Error as Server Data</h3>;
+	}
+
+	if (!data) {
+		return <h3>No data</h3>;
+	}
+
 	return (
 		<>
-			<Container>
-				LOLKEK
-				<CoinsTable data={DataCoins} />
+			<Container className='d-flex align-items-center justify-content-center text-center not-found-container mt-5 '>
+				<Row>
+					<Col>
+						<CoinsTable data={data} />
+					</Col>
+				</Row>
 			</Container>
 		</>
 	);
--- src/components/CoinsTable.tsx
+++ src/components/CoinsTable.tsx
@@ -3,30 +3,31 @@ import { Table } from 'react-bootstrap';
 
 export const CoinsTable: React.FC = ({ data }) => {
 	return (
-		<Table>
+		<Table striped bordered hover>
 			<thead>
 				<tr>
 					<th>№</th>
-					<th>Название</th>
-					<th>Цена</th>
+					<th>Name</th>
+					<th>Price</th>
 				</tr>
 			</thead>
 			<tbody>
-				{data.map(obj => (
-					<tr>
-						<td>{obj.rank}</td>
-						<td>
-							<img
-								src={obj.icon}
-								alt={obj.rank}
-								width={20}
-								style={{ marginRight: 10 }}
-							/>
-							{obj.name}
-						</td>
-						<td>$ {obj.price}</td>
-					</tr>
-				))}
+				{data &&
+					data.map(obj => (
+						<tr key={obj.id}>
+							<td>{obj.rank}</td>
+							<td>
+								<img
+									src={obj.icon}
+									alt={obj.rank}
+									width={20}
+									style={{ marginRight: 10 }}
+								/>
+								{obj.name}
+							</td>
+							<td>$ {obj.price}</td>
+						</tr>
+					))}
 			</tbody>
 		</Table>
 	);
